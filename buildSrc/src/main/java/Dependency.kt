a5cb8ecf71fb7e5f62ed01c0a562a9ea956bffb9
guen <guen5997@naver.com>
2023-11-30 17:36:12 +0900

    fix: 종속성 호환성

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,7 +1,7 @@
 import Versions.NAV_VERSION
 
 object Versions {
-    const val NAV_VERSION = "2.4.0-alpha10"
+    const val NAV_VERSION = "2.6.0-alpha03"
 
 }
 
@@ -10,29 +10,34 @@ object Kotlin {
 }
 
 object AndroidX {
-    const val MATERIAL = "androidx.compose.material:material:1.0.0-rc02"
-    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.0"
-    const val APP_COMPAT = "androidx.appcompat:appcompat:1.3.1"
-    const val LEGACY = "androidx.legacy:legacy-support-v4:1.0.0"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.2"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.6.1"
     const val LIFECYCLE_VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0"
     const val LIFECYCLE_LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.1"
-    const val ACTIVITY = "androidx.activity:activity-ktx:1.3.1"
-    const val FRAGMENT = "androidx.fragment:fragment-ktx:1.5.2"
-    const val DATASTORE = "androidx.datastore:datastore-preferences:1.0.0"
+    const val ACTIVITY_VERSION = "androidx.activity:activity-ktx:1.4.0-alpha01"
 }
 
 object KTX {
-    const val CORE = "androidx.core:core-ktx:1.6.0"
+    const val CORE = "androidx.core:core-ktx:1.7.0-alpha02"
 }
 
 object Google {
-    const val MATERIAL = "com.google.android.material:material:1.11.0-alpha01"
+    const val MATERIAL = "com.google.android.material:material:1.6.1"
 }
 
 object Test {
     const val JUNIT = "junit:junit:4.+"
     const val ANDROID_JUNIT_RUNNER = "AndroidJUnitRunner"
 }
+
+object Glide {
+    const val GLIDE_VERSION = "com.github.bumptech.glide:glide:4.12.0"
+}
+
+object Gson {
+    const val GSON_VERSION = "com.google.code.gson:gson:2.8.9"
+}
+
 
 object AndroidTest {
     const val EXT_JUNIT = "androidx.test.ext:junit:1.1.3"
@@ -60,16 +65,14 @@ object OkHttp {
 }
 
 object Coroutines {
-    const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2"
-    const val COROUTINES_PLAY_SERVICES = "org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.1.1"
+    const val COROUTINES = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9"
 }
 
 object NavComponent {
     const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:$NAV_VERSION"
     const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:$NAV_VERSION"
-    const val NAVIGATION_DYNAMIC_FEATURES_FRAGMENT = "androidx.navigation:navigation-dynamic-features-fragment:$NAV_VERSION"
-    const val NAVIGATION_TESTING = "androidx.navigation:navigation-testing:$NAV_VERSION"
-    const val NAVIGATION_KOTLIN = "androidx.fragment:fragment-ktx:1.4.1:$NAV_VERSION"
+    const val NAVIGATION_FEATURE = "androidx.navigation:navigation-dynamic-features-fragment:$NAV_VERSION"
+    const val NAVIGATION_KOTLIN = "androidx.fragment:fragment-ktx:1.5.4"
 }
 
 object TestTool {
