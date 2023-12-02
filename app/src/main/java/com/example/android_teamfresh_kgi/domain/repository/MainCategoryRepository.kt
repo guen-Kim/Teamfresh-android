41e989bb771e6dfdc9cbb1733cfebb5ccfb84720
guen <guen5997@naver.com>
2023-12-02 12:48:34 +0900

    Update:fun checkMajorCategory

--- app/src/main/java/com/example/android_teamfresh_kgi/domain/repository/MainCategoryRepository.kt
+++ app/src/main/java/com/example/android_teamfresh_kgi/domain/repository/MainCategoryRepository.kt
@@ -7,7 +7,7 @@ import com.example.android_teamfresh_kgi.domain.utils.RemoteErrorEmitter
 
 // 페이지1 대분류, 퀵메뉴 dataSource 비지니스 로직
 interface MainCategoryRepository {
-    suspend fun checkMainCategory(remoteErrorEmitter: RemoteErrorEmitter): DomainMajorCategoryResponse?
+    suspend fun checkMajorCategory(remoteErrorEmitter: RemoteErrorEmitter): DomainMajorCategoryResponse?
     suspend fun checkQuickMenu(remoteErrorEmitter: RemoteErrorEmitter): DomainQuickMenuResponse?
 
 }
