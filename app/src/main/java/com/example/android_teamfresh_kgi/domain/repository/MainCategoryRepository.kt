125b3d6d4752be7ad74a4dcd9229ab361db1e770
guen <guen5997@naver.com>
2023-12-02 12:22:20 +0900

    Create: CategoryRepository interface

---
+++ app/src/main/java/com/example/android_teamfresh_kgi/domain/repository/MainCategoryRepository.kt
@@ -0,0 +1,13 @@
+package com.example.android_teamfresh_kgi.domain.repository
+
+import com.example.android_teamfresh_kgi.domain.model.DomainMajorCategoryResponse
+import com.example.android_teamfresh_kgi.domain.model.DomainQuickMenuResponse
+import com.example.android_teamfresh_kgi.domain.utils.RemoteErrorEmitter
+
+
+// 페이지1 대분류, 퀵메뉴 dataSource 비지니스 로직
+interface MainCategoryRepository {
+    suspend fun checkMainCategory(remoteErrorEmitter: RemoteErrorEmitter): DomainMajorCategoryResponse?
+    suspend fun checkQuickMenu(remoteErrorEmitter: RemoteErrorEmitter): DomainQuickMenuResponse?
+
+}
