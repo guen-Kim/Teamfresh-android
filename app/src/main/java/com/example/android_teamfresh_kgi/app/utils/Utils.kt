78f6f90dcb811ca3798cbc8129f759db5dea471a
guen <guen5997@naver.com>
2023-12-02 18:13:52 +0900

    fix: base url

--- app/src/main/java/com/example/android_teamfresh_kgi/app/utils/Utils.kt
+++ app/src/main/java/com/example/android_teamfresh_kgi/app/utils/Utils.kt
@@ -1,5 +1,5 @@
 package com.example.android_teamfresh_kgi.app.utils
 
 object Utils {
-    const val BASE_URL ="https://smapistage.teamfresh.co.kr/"
+    const val BASE_URL ="https://smapistage.teamfresh.co.kr"
 }
