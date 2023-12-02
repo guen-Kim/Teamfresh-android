b92b6acd4f1be8de0a335b2465be74f6db3897e2
guen <guen5997@naver.com>
2023-12-02 18:15:42 +0900

    Update: Mapper - data Model to domain

--- app/src/main/java/com/example/android_teamfresh_kgi/data/mapper/Mapper.kt
+++ app/src/main/java/com/example/android_teamfresh_kgi/data/mapper/Mapper.kt
@@ -1,21 +1,31 @@
 package com.example.android_teamfresh_kgi.data.mapper
 
-import com.example.android_teamfresh_kgi.data.model.MajorCartegoryResponse
+import com.example.android_teamfresh_kgi.data.model.MajorCategoryResponse
 import com.example.android_teamfresh_kgi.data.model.QuickMenuResponse
+import com.example.android_teamfresh_kgi.domain.model.DomainDispClasItem
 import com.example.android_teamfresh_kgi.domain.model.DomainMajorCategoryResponse
+import com.example.android_teamfresh_kgi.domain.model.DomainMajorDispClasItem
 import com.example.android_teamfresh_kgi.domain.model.DomainQuickMenuResponse
 
 /* data Model to domain Model */
-
 object Mapper {
 
-    fun MajorCategoryMapper(majorCartegoryResponse: MajorCartegoryResponse?): DomainMajorCategoryResponse? {
+    fun MajorCategoryMapper(majorCartegoryResponse: MajorCategoryResponse?): DomainMajorCategoryResponse? {
         return if (majorCartegoryResponse != null) {
+            val data = mutableListOf<DomainMajorDispClasItem>()
+            majorCartegoryResponse.data.forEach {
+                data.add(
+                    DomainMajorDispClasItem(
+                        it.dispClasSeq,
+                        it.dispClasNm,
+                        it.dispClasImgPath,
+                        it.dispClasCd,
+                    )
+                )
+            }
             DomainMajorCategoryResponse(
-                dispClasSeq = majorCartegoryResponse.dispClasSeq,
-                dispClasNm = majorCartegoryResponse.dispClasNm,
-                dispClasImgPath = majorCartegoryResponse.dispClasImgPath,
-                dispClasCd = majorCartegoryResponse.dispClasCd,
+                data = data,
+                message = majorCartegoryResponse.message,
             )
         } else {
             majorCartegoryResponse
@@ -24,13 +34,23 @@ object Mapper {
 
     fun QuickMenuResponseMapper(quickMenuResponse: QuickMenuResponse?): DomainQuickMenuResponse? {
         return if (quickMenuResponse != null) {
+
+            val data = mutableListOf<DomainDispClasItem>()
+            quickMenuResponse.data.forEach {
+                data.add(
+                    DomainDispClasItem(
+                        it.quickMenuSeq,
+                        it.quickMenuNm,
+                        it.quickMenuImgPath,
+                        it.quickMenuConcScrenNm,
+                        it.quickMenuConcScrenIden,
+                        it.quickMenuMovScrenPath
+                    )
+                )
+            }
             DomainQuickMenuResponse(
-                quickMenuConcScrenIden = quickMenuResponse.quickMenuConcScrenIden,
-                quickMenuConcScrenNm = quickMenuResponse.quickMenuConcScrenNm,
-                quickMenuImgPath = quickMenuResponse.quickMenuImgPath,
-                quickMenuMovScrenPath = quickMenuResponse.quickMenuMovScrenPath,
-                quickMenuNm = quickMenuResponse.quickMenuNm,
-                quickMenuSeq = quickMenuResponse.quickMenuSeq,
+                data = data,
+                message = quickMenuResponse.message,
             )
         } else {
             quickMenuResponse
