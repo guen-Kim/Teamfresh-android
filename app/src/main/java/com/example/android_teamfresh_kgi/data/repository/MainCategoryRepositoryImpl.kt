2c52032c4dbc56cc0e9f31c2625b2be100bd3f4e
guen <guen5997@naver.com>
2023-12-02 12:23:47 +0900

    add: MainCategoryRepositoryImpl

---
+++ app/src/main/java/com/example/android_teamfresh_kgi/data/repository/MainCategoryRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.example.android_teamfresh_kgi.data.repository
+
+import com.example.android_teamfresh_kgi.data.mapper.Mapper
+import com.example.android_teamfresh_kgi.data.repository.remote.datasource.MajorCategoryDataSource
+import com.example.android_teamfresh_kgi.data.repository.remote.datasource.QuickMenuDataSource
+import com.example.android_teamfresh_kgi.domain.model.DomainMajorCategoryResponse
+import com.example.android_teamfresh_kgi.domain.model.DomainQuickMenuResponse
+import com.example.android_teamfresh_kgi.domain.repository.MainCategoryRepository
+import com.example.android_teamfresh_kgi.domain.utils.RemoteErrorEmitter
+
+
+/* Repository Pattern */
+class MainCategoryRepositoryImpl(
+    private val majorCategoryDataSource: MajorCategoryDataSource,
+    private val quickMenuDataSource: QuickMenuDataSource
+) :
+    MainCategoryRepository {
+    override suspend fun checkMainCategory(remoteErrorEmitter: RemoteErrorEmitter): DomainMajorCategoryResponse? {
+        return Mapper.MajorCategoryMapper(
+            majorCategoryDataSource.checkMajorCategory(remoteErrorEmitter)
+        )
+    }
+
+    override suspend fun checkQuickMenu(remoteErrorEmitter: RemoteErrorEmitter): DomainQuickMenuResponse? {
+        return Mapper.QuickMenuResponseMapper(
+            quickMenuDataSource.checkQuickMenuDataSource(remoteErrorEmitter)
+        )
+    }
+}
