adaef4b07315e8b8573c95f18439694b00fbf244
guen <guen5997@naver.com>
2023-12-02 12:21:36 +0900

    add: Domain Model(MajorCategory, QuickMenu)

---
+++ app/src/main/java/com/example/android_teamfresh_kgi/domain/model/DomainMajorCategoryResponse.kt
@@ -0,0 +1,8 @@
+package com.example.android_teamfresh_kgi.domain.model
+
+data class DomainMajorCategoryResponse (
+    val dispClasSeq: Int,
+    val dispClasNm: String,
+    val dispClasImgPath: String,
+    val dispClasCd:String,
+)
---
+++ app/src/main/java/com/example/android_teamfresh_kgi/domain/model/DomainQuickMenuResponse.kt
@@ -0,0 +1,11 @@
+package com.example.android_teamfresh_kgi.domain.model
+
+data class DomainQuickMenuResponse (
+    val quickMenuConcScrenIden: String,
+    val quickMenuConcScrenNm: String,
+    val quickMenuImgPath: String,
+    val quickMenuMovScrenPath: String,
+    val quickMenuNm: String,
+    val quickMenuSeq: Int,
+
+)
